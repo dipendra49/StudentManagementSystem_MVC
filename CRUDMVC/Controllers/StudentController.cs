d1b66edb57083be50b05dbff15d4a695374a557d
dipendra49 <chaudharydipendra49@gmail.com>
2022-06-08 15:58:39 +0545

    First commit

---
+++ CRUDMVC/Controllers/StudentController.cs
@@ -0,0 +1,107 @@
+﻿using CRUDMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CRUDMVC.Controllers
+{
+    public class StudentController : Controller
+    {
+        CrudMVCEntities dbObj = new CrudMVCEntities();
+
+        
+        public ActionResult Details()
+        {
+            List<tbl_Student> student = dbObj.tbl_Student.ToList();
+            return View(student);
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ActionName("Create")]
+        public ActionResult Create_Post()
+        {
+            if (ModelState.IsValid)
+            {
+                tbl_Student std = new tbl_Student();
+                TryUpdateModel(std);
+                dbObj.tbl_Student.Add(std);
+                TempData["AddMessage"] = "New student has been added successfully";
+                dbObj.SaveChanges();
+                return RedirectToAction("Details");
+                //std.Name = student.Name;
+                //std.Course = student.Course;
+                //std.Gender = student.Gender;
+                //std.Address = student.Address;
+                //std.Email = student.Email;
+            }
+            ModelState.Clear();
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            tbl_Student student = dbObj.tbl_Student.Single(m => m.ID == id);
+            return View(student);
+        }
+        [HttpPost]
+        [ActionName("Edit")]
+        public ActionResult Edit_Post(tbl_Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                UpdateDetail detail = new UpdateDetail();
+                detail.Update(student);
+                TempData["UpdateMessage"] = "Data has been updated successfully";
+                return RedirectToAction("Details");
+                //bool check = detail.Update(student);
+                //if (check == true)
+                //{
+                //ModelState.Clear();
+                //return RedirectToAction("Details");
+
+                //}
+
+                //tbl_Student std = new tbl_Student();
+                //TryUpdateModel(std);
+                //dbObj.tbl_Student.Add(std);
+            }
+            return View(student);
+        }
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            tbl_Student student = dbObj.tbl_Student.Single(m => m.ID == id);
+            return View(student);
+        }
+
+
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult Delete_Post(int id)
+        {
+            if (ModelState.IsValid)
+            {
+                UpdateDetail delete = new UpdateDetail();
+                delete.DeleteRecord(id);
+                TempData["DeleteMessage"] = "Data deleted successfully!!";
+                return RedirectToAction("Details");
+            }
+            return View();
+        }
+
+        public ActionResult Detail(int id)
+        {
+            tbl_Student student = dbObj.tbl_Student.Single(m => m.ID == id);
+            return View(student);
+        }
+
+
+    }
+}
---
+++ CRUDMVC/Models/UpdateDetail.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace CRUDMVC.Models
+{
+    public class UpdateDetail
+    {
+        public void Update(tbl_Student student)
+        {
+            string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;Initial Catalog=CrudMVC;Integrated Security=True;MultipleActiveResultSets=True;Application Name=EntityFramework";
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                string query = "UPDATE tbl_Student SET Name = @name, Course = @course, Gender = @gender, Address = @address, Email = @email WHERE ID = @id ";
+
+                SqlCommand cmd = new SqlCommand(query, conn);
+
+                //SqlParameter paraID = new SqlParameter();
+                //paraID.ParameterName = "@id";
+                //paraID.Value = student.ID;
+                //cmd.Parameters.Add(paraID);
+
+                //SqlParameter paraName = new SqlParameter();
+                //paraName.ParameterName = "@name";
+                //paraName.Value = student.Name;
+                //cmd.Parameters.Add(paraName);
+
+                //SqlParameter paraCourse = new SqlParameter();
+                //paraCourse.ParameterName = "@course";
+                //paraCourse.Value = student.Course;
+                //cmd.Parameters.Add(paraCourse);
+
+                //SqlParameter paraGender = new SqlParameter();
+                //paraGender.ParameterName = "@gender";
+                //paraGender.Value = student.Gender;
+                //cmd.Parameters.Add(paraGender);
+
+                //SqlParameter paraAddress = new SqlParameter();
+                //paraAddress.ParameterName = "@address";
+                //paraAddress.Value = student.Address;
+                //cmd.Parameters.Add(paraAddress);
+
+                //SqlParameter paraEmail = new SqlParameter();
+                //paraEmail.ParameterName = "@email";
+                //paraEmail.Value = student.Email;
+                //cmd.Parameters.Add(paraEmail);
+
+                cmd.Parameters.AddWithValue("@id", student.ID);
+                cmd.Parameters.AddWithValue("@name", student.Name);
+                cmd.Parameters.AddWithValue("@course", student.Course);
+                cmd.Parameters.AddWithValue("@gender", student.Gender);
+                cmd.Parameters.AddWithValue("@address", student.Address);
+                cmd.Parameters.AddWithValue("@email", student.Email);
+
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                //int i = cmd.ExecuteNonQuery();
+                //conn.Close();
+
+                //if (i>0)
+                //{
+                //    return true;
+                //}
+                //else
+                //{
+                //    return false;
+                //}
+
+
+
+
+            }
+        }
+
+        public void DeleteRecord(int id)
+        {
+            string cs = @"Data Source=(LocalDB)\MSSQLLocalDB;Initial Catalog=CrudMVC;Integrated Security=True;MultipleActiveResultSets=True;Application Name=EntityFramework";
+
+            using (SqlConnection conn = new SqlConnection(cs))
+            {
+                string query = "DELETE FROM tbl_Student WHERE ID = @id";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@id", id);
+                conn.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        public void DisplayRecord(int id)
+        {
+            string cs = @"Data Source=(LocalDB)\MSSQLLocalDB;Initial Catalog=CrudMVC;Integrated Security=True;MultipleActiveResultSets=True;Application Name=EntityFramework";
+
+            using (SqlConnection conn = new SqlConnection(cs))
+            {
+                string query = "SELECT Name, Course, Gender, Address, Email FROM tbl_Student WHERE ID = @id";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@id", id);
+                conn.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+    }
+}
